e332ef626760420935f94f6fa6fdd4590a90a935
MATHEUS HENRIQUE DE SOUZA VICENTE <matheus.vicente@alunos.sc.senac.br>
2022-03-14 21:51:12 -0300

    Requirido

--- JovemProgramadorMvc/Controllers/AlunoController.cs
+++ JovemProgramadorMvc/Controllers/AlunoController.cs
@@ -1,16 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JovemProgramadorMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace JovemProgramadorMvc.Controllers
 {
     public class AlunoController : Controller 
     {
-        public AlunoController()
+        private readonly IConfiguration _configuration;
+        
+        
+        public AlunoController(IConfiguration configuration)
         {
-
+            _configuration = configuration;
         }
         public IActionResult Index()
         {
@@ -24,9 +31,28 @@ namespace JovemProgramadorMvc.Controllers
         {
             return View();
         }
-        public IActionResult BuscarCep(string cep)
+        public async Task<IActionResult> BuscarCep(string cep)
         {
-            return View();
+            EnderecoModel enderecoModel = new();
+
+            try
+            {
+                cep = cep.Replace("-", "");
+
+                using var client = new HttpClient();
+                var result = await client.GetAsync(_configuration.GetSection("ApiCep")["BaseUrl"] + cep + "/json");
+                if(result.IsSuccessStatusCode)
+                {
+                    enderecoModel = JsonSerializer.Deserialize<EnderecoModel> (
+                        await result.Content.ReadAsStringAsync(), new JsonSerializerOptions() { });
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            
+            return View("Buscarcep", enderecoModel);
         }
     }
 }
